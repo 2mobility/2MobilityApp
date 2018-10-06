3f4ac4a103ae88f6d60b1cb8f571061f4cdc2f10
2mobility <2mobilityapp@gmail.com>
2018-10-06 13:39:39 -0300

    - Gráficos...

--- app/src/main/java/com/mobility/a2mobilityapp/project/view/GraficoFinanceiroFragment.java
+++ app/src/main/java/com/mobility/a2mobilityapp/project/view/GraficoFinanceiroFragment.java
@@ -1,6 +1,7 @@
 package com.mobility.a2mobilityapp.project.view;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -8,7 +9,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.github.mikephil.charting.animation.Easing;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
 import com.mobility.a2mobilityapp.R;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -27,6 +36,8 @@ public class GraficoFinanceiroFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    PieChart pieChart;
 
     private OnFragmentInteractionListener mListener;
 
@@ -59,13 +70,22 @@ public class GraficoFinanceiroFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_grafico_financeiro, container, false);
+
+
+        View view = inflater.inflate(R.layout.fragment_grafico_financeiro, container, false);
+
+        pieChart = view.findViewById(R.id.piechart_1);
+
+        setPieChart(30,30,30);
+
+        return view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -105,5 +125,30 @@ public class GraficoFinanceiroFragment extends Fragment {
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
+    }
+
+    //Grafico dinheiro
+    public void setPieChart(int transportePublico, int uber, int carroProprio) {
+        this.pieChart = pieChart;
+        pieChart.setUsePercentValues(true);
+        pieChart.getDescription().setEnabled(true);
+        pieChart.setExtraOffsets(5,10,5,5);
+        pieChart.setDragDecelerationFrictionCoef(0.9f);
+        pieChart.setTransparentCircleRadius(61f);
+        pieChart.setHoleColor(Color.WHITE);
+        pieChart.animateY(1000, Easing.EasingOption.EaseInOutCubic);
+        ArrayList<PieEntry> yValues = new ArrayList<>();
+        yValues.add(new PieEntry(transportePublico,"Transporte Público"));
+        yValues.add(new PieEntry(uber,"Uber"));
+        yValues.add(new PieEntry(carroProprio,"Carro Próprio"));
+
+        PieDataSet dataSet = new PieDataSet(yValues, "Dinheiro");
+        dataSet.setSliceSpace(3f);
+        dataSet.setSelectionShift(5f);
+        dataSet.setColors(ColorTemplate.MATERIAL_COLORS);
+        PieData pieData = new PieData((dataSet));
+        pieData.setValueTextSize(10f);
+        pieData.setValueTextColor(Color.BLACK);
+        pieChart.setData(pieData);
     }
 }
--- app/src/main/java/com/mobility/a2mobilityapp/project/view/GraficoTempoFragment.java
+++ app/src/main/java/com/mobility/a2mobilityapp/project/view/GraficoTempoFragment.java
@@ -1,6 +1,7 @@
 package com.mobility.a2mobilityapp.project.view;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -8,7 +9,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.github.mikephil.charting.animation.Easing;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
 import com.mobility.a2mobilityapp.R;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -29,6 +38,8 @@ public class GraficoTempoFragment extends Fragment {
     private String mParam2;
 
     private OnFragmentInteractionListener mListener;
+
+    PieChart pieChart;
 
     public GraficoTempoFragment() {
         // Required empty public constructor
@@ -65,7 +76,13 @@ public class GraficoTempoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_grafico_tempo, container, false);
+        View view = inflater.inflate(R.layout.fragment_grafico_tempo, container, false);
+
+        pieChart = view.findViewById(R.id.piechart_1);
+
+        setPieChart(30,30,30);
+
+        return view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -105,5 +122,30 @@ public class GraficoTempoFragment extends Fragment {
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
+    }
+
+    //Grafico Tempo
+    public void setPieChart(int transportePublico, int uber, int carroProprio) {
+        this.pieChart = pieChart;
+        pieChart.setUsePercentValues(true);
+        pieChart.getDescription().setEnabled(true);
+        pieChart.setExtraOffsets(5,10,5,5);
+        pieChart.setDragDecelerationFrictionCoef(0.9f);
+        pieChart.setTransparentCircleRadius(61f);
+        pieChart.setHoleColor(Color.WHITE);
+        pieChart.animateY(1000, Easing.EasingOption.EaseInOutCubic);
+        ArrayList<PieEntry> yValues = new ArrayList<>();
+        yValues.add(new PieEntry(transportePublico,"Transporte Público"));
+        yValues.add(new PieEntry(uber,"Uber"));
+        yValues.add(new PieEntry(carroProprio,"Carro Próprio"));
+
+        PieDataSet dataSet = new PieDataSet(yValues, "Tempo");
+        dataSet.setSliceSpace(3f);
+        dataSet.setSelectionShift(5f);
+        dataSet.setColors(ColorTemplate.MATERIAL_COLORS);
+        PieData pieData = new PieData((dataSet));
+        pieData.setValueTextSize(10f);
+        pieData.setValueTextColor(Color.BLACK);
+        pieChart.setData(pieData);
     }
 }
