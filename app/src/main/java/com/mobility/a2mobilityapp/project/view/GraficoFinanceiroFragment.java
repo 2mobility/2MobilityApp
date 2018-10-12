896f9ac47dedf714f8da6925789b0253d258ad3e
2mobility <2mobilityapp@gmail.com>
2018-10-12 16:17:30 -0300

    - Populando Spinner dos gráficos... - Pegando item selecionado e mudando os gráficos...

--- app/src/main/java/com/mobility/a2mobilityapp/project/view/GraficoFinanceiroFragment.java
+++ app/src/main/java/com/mobility/a2mobilityapp/project/view/GraficoFinanceiroFragment.java
@@ -6,7 +6,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.mobility.a2mobilityapp.R;
 
@@ -36,16 +39,53 @@ public class GraficoFinanceiroFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_grafico_financeiro, container, false);
 
         pieChart = view.findViewById(R.id.piechart_1);
         comboBox = view.findViewById(R.id.spinnerCusto);
 
-        setPieChart(30,30,30);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_spinner_dropdown_item, dias);
+        comboBox.setAdapter(arrayAdapter);
+
+        //Método do Spinner para capturar o item selecionado
+        comboBox.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View v, int posicao, long id) {
+                //pega nome pela posição
+                String nome = parent.getItemAtPosition(posicao).toString();
+                if(nome.equals("Diário")){
+                    int valorCalculo = 1;
+                    int valorTransportePublico = 10;
+                    int valorUber = 20;
+                    int valorCarroProprio = 30;
+                    //TransportePublico, Uber, CarroProprio
+                    setPieChart((valorTransportePublico*valorCalculo),(valorUber*valorCalculo),(valorCarroProprio*valorCalculo));
+                }
+                if(nome.equals("Semanal")){
+                    int valorCalculo = 7;
+                    int valorTransportePublico = 5;
+                    int valorUber = 15;
+                    int valorCarroProprio = 25;
+                    //TransportePublico, Uber, CarroProprio
+                    setPieChart((valorTransportePublico*valorCalculo),(valorUber*valorCalculo),(valorCarroProprio*valorCalculo));
+                }
+                if(nome.equals("Mensal")){
+                    int valorCalculo = 30;
+                    int valorTransportePublico = 20;
+                    int valorUber = 40;
+                    int valorCarroProprio = 60;
+                    //TransportePublico, Uber, CarroProprio
+                    setPieChart((valorTransportePublico*valorCalculo),(valorUber*valorCalculo),(valorCarroProprio*valorCalculo));
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
         return view;
     }
